ddb7bfda31fe3d9709618d69e3a8c6632fac233d
Dika Jefrianto <145889310+DikaJefrianto@users.noreply.github.com>
2025-11-30 22:33:13 +0700

    Update BASE_URL to production API endpoint

--- src/api/transaksiApi.js
+++ src/api/transaksiApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:5000/api/transaksi";
+const BASE_URL = "https://ideal-commitment-production.up.railway.app/api/transaksi";
 
 export const getTransactions = async () => {
   const res = await axios.get(BASE_URL);
